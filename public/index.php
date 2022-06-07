8f294a01b2fd9a7f94cb333dee95be1fdadc7332
raulmtm64 <raulmtm64@gmail.com>
2022-06-07 19:54:30 +0200

    Eliminar htaccess y pruebas

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@ header('Access-Control-Allow-Origin: *');
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-require '../src/config/db.php';
+// require '../vendor/autoload.php';
+// require '../src/config/db.php';
 
 $app = new \Slim\App;
 $app->get('/', function (Request $request, Response $response, array $args) {
@@ -18,6 +18,6 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 });
 
 // Rutas de los archivos
-require '../src/routes/login.php';
+// require '../src/routes/login.php';
 
 $app->run();
